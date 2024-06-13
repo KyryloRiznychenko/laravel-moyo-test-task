db7ae041facbaabc35ec656370d7836b7a3763a3
Гістьь <gist@Yelyzavetas-MacBook-Pro.local>
2024-06-13 13:33:07 +0300

    init project

---
+++ docker-compose/php/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+
+cd /var/www
+
+composer install
+
+if [ "$APP_ENV" = 'production' ]; then
+    echo "IS RUNNING PRODUCTION MODE"
+    cp .env.example .env
+else
+    echo "IS RUNNING LOCAL-DEVELOPMENT MODE"
+
+    if [ ! -e "./database/database.sqlite" ]; then
+        touch "./database/database.sqlite"
+        echo "The database.sqlite file has been created for local development"
+    fi
+
+    cp .env.example.local .env
+    php artisan migrate
+fi
+
+php artisan key:generate
+
+php-fpm
